f49891311b499d16d5d32e800c257a5c3289709e
Yuanlong Tan <yt4xb@virginia.edu>
2018-10-26 13:44:20 -0400

    Test for new date convert

--- ldm7-rtstats.py
+++ ldm7-rtstats.py
@@ -96,8 +96,9 @@ def parseBackstop(feedtype, line):
 			# col 5 is size
 			size = int(split_line[5])
 			# col 0 is the arrival time, col 6 is the insertion time.
-			arrival_time = parse(split_line[0]).astimezone(pytz.utc).replace(tzinfo=None)
-			insert_time = datetime.strptime(split_line[6], "%Y%m%d%H%M%S.%f")
+			# arrival_time = parse(split_line[0]).astimezone(pytz.utc).replace(tzinfo=None)
+            arrival_time = datetime.strptime(split_line[0], "%Y%m%dT%H%M%S.%f")
+            insert_time = datetime.strptime(split_line[6], "%Y%m%d%H%M%S.%f")
 			rxtime = (arrival_time - insert_time).total_seconds()
 			return (prodindex, size, rxtime)
 		else:
