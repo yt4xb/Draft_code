a7261ca1598bddc5f62ac2c3c27b8f4b329d088b
Yuanlong Tan <yt4xb@virginia.edu>
2018-11-02 10:06:08 -0400

    upload decoder.py & modify hearders

--- deploy_LDM7.sh
+++ deploy_LDM7.sh
@@ -1,26 +1,5 @@
 # -*- coding: utf-8 -*-
 
-"""
-Copyright (C) 2018 University of Virginia. All rights reserved.
-
-file      LDM7_deploy.sh
-author    Yuanlong Tan <yt4xb@virginia.edu>
-version   1.0
-date      Oct. 22, 2018
-
-LICENSE
-This program is free software; you can redistribute it and/or modify it
-under the terms of the GNU General Public License as published by the Free
-Software Foundation; either version 2 of the License, or（at your option）
-any later version.
-
-This program is distributed in the hope that it will be useful, but WITHOUT
-ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
-FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for
-more details at http://www.gnu.org/copyleft/gpl.html
-
-brief     Script for installing LDM7
-"""
 sudo apt-get install -y libxml2-dev
 sudo apt-get install -y libpng-dev
 sudo apt-get install -y zlib1g-dev
--- ldm7-rtstats.sh
+++ ldm7-rtstats.sh
@@ -1,6 +1,4 @@
 #EXEC file to create & send ldm7-rtstats.log
-cat /home/ldm7/var/logs/ldmd.log | grep mldm_receiver.c | awk '{print $1"\t"$8"\t"$7"\t"$5"\t"$9}' > ldm7-rtstats-fdt.csv
-ldmadmin newlog
-python parse.py ldm7-rtstats-fdt.csv ldm7-rtstats-fdt.log
+python ldm7-rtstats.py /home/ldm7/var/logs/ldmd.log ldm7-rtstats-fdt.log
 ldmsend -v -h idc-uva.dynes.virginia.edu -P 38800 ldm7-rtstats-fdt.log
 sleep 60
