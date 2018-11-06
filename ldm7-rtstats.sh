cac51d6254019309400cad57eba5434ce2437b99
Yuanlong Tan <yt4xb@virginia.edu>
2018-11-06 13:14:23 -0500

    add "While" loop

--- ldm7-rtstats.sh
+++ ldm7-rtstats.sh
@@ -1,4 +1,7 @@
 #EXEC file to create & send ldm7-rtstats.log
-python ldm7-rtstats.py var/logs/ldmd.log ldm7-rtstats.log
-ldmsend -v -h idc-uva.dynes.virginia.edu -P 38800 ldm7-rtstats.log
-sleep 60
+while true
+do
+  python ldm7-rtstats.py var/logs/ldmd.log ldm7-rtstats.log
+  ldmsend -v -h idc-uva.dynes.virginia.edu -P 38800 ldm7-rtstats.log
+  sleep 60
+done
