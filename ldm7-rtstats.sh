0e77c469b155e1326f3d00bc3c4b6c3d6fe2a7b1
Yuanlong Tan <yt4xb@virginia.edu>
2018-11-29 19:42:23 -0500

    sed utility to solve the problem of long time response

--- ldm7-rtstats.sh
+++ ldm7-rtstats.sh
@@ -1,7 +1,9 @@
 #EXEC file to create & send ldm7-rtstats.log
 while true
 do
-  python util/ldm7-rtstats.py var/logs/ldmd.log ldm7-rtstats.log
-  ldmsend -v -h idc-uva.dynes.virginia.edu -P 38800 ldm7-rtstats.log
+  LastTimeStamp=$(tail -1 var/logs/ldmd.log | awk '{print $1}')
   sleep 60
+  sed -n "/$LastTimeStamp/",'$p' var/logs/ldmd.log | sed -n '2,$p' >& ldm7.log
+  python util/ldm7-rtstats.py ldm7.log ldm7-rtstats.log
+  ldmsend -v -h idc-uva.dynes.virginia.edu -P 38800 ldm7-rtstats.log
 done
