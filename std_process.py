2ebd881d85496aa7e2e66e3742b5293f7dbd3f6e
Yuanlong <yt4xb@virginia.edu>
2019-01-10 21:00:26 -0500

    modified:   std_process.py

--- std_process.py
+++ std_process.py
@@ -24,23 +24,27 @@ from datetime import datetime, timedelta
 
 def parse(start_time, end_time, line):
     split_line = line.split()
-    time = datetime.strptime(split_line[1], "%Y%m%dT%H%M%S.%fZ")
-    if time > start_time && time < end_time:
-    	size = int(split_line[0])
-    	return (size, time)
-    else:
-    	return (-1, -1)
+    time = datetime.strptime(split_line[1], "%Y%m%d%H%M%S.%f")
+    if time > start_time: 
+	if time < end_time:
+    		size = int(split_line[0])
+    		return (size, time)
+	else: 
+		return (-1, -1)
+    else: 
+	return (-1, -1)
 
 def main(logfile, csvfile):
 
-    start_time = datetime.strptime("20181114130000.000000", "%Y%m%dT%H%M%S.%f")
-    end_time = datetime.strptime("20181114140000.000000", "%Y%m%dT%H%M%S.%f")
+    start_time = datetime.strptime("20181114130000.000000", "%Y%m%d%H%M%S.%f")
+    end_time = datetime.strptime("20181114140000.000000", "%Y%m%d%H%M%S.%f")
     w = open(csvfile, 'w+')
-    r = open(filename, 'r')
-    for i, line in enumerate(logfile):
-        (size, time) = parse(start_time, end_time, line)
-        tmp_str = str(time) + ',' + str(complete_size) + '\n'
-        w.write(tmp_str)
+    with open(logfile, 'r') as r:
+    	for i, line in enumerate(r):
+        	(size, time) = parse(start_time, end_time, line)
+       		if size > 0:
+			tmp_str = str(time) + ',' + str(complete_size) + '\n'
+        		w.write(tmp_str)
     r.close()
     w.close()
 
