1606043c3e42c87d3c2eb12d038b886228a5815b
Yuanlong Tan <yt4xb@virginia.edu>
2019-03-11 14:44:46 -0400

    added for third argv

--- ldm7-rtstats.sh
+++ ldm7-rtstats.sh
@@ -4,7 +4,6 @@ do
   LastTimeStamp=$(tail -1 var/logs/ldmd.log | awk '{print $1}')
   sleep 60
   sed -n "/$LastTimeStamp/",'$p' var/logs/ldmd.log | sed -n '2,$p' >& ldm7.log
-  python util/ldm7-rtstats.py ldm7.log ldm7-rtstats.log
+  python util/ldm7-rtstats.py ldm7.log ldm7-rtstats.log NGRID
   ldmsend -v -h idc-uva.dynes.virginia.edu -P 38800 ldm7-rtstats.log
 done
-
