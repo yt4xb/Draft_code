1035d9f1e46dddfcc1dd760d6f9f0e7830ece1e4
Yuanlong Tan <yt4xb@virginia.edu>
2019-03-13 18:24:33 -0400

    more efficient

--- ldm7-rtstats.sh
+++ ldm7-rtstats.sh
@@ -3,7 +3,7 @@ while true
 do
   LastTimeStamp=$(tail -1 var/logs/ldmd.log | awk '{print $1}')
   sleep 60
-  sed -n "/$LastTimeStamp/",'$p' var/logs/ldmd.log | sed -n '2,$p' >& ldm7.log
+  sed -n "/$LastTimeStamp/",'$p' var/logs/ldmd.log | grep -E "Received:|Inserted:" | sed -n '2,$p' >& ldm7.log
   python util/ldm7-rtstats.py ldm7.log ldm7-rtstats.log NGRID
   ldmsend -v -h idc-uva.dynes.virginia.edu -P 38800 ldm7-rtstats.log
 done
