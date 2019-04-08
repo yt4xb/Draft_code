7f3136232c780518120ec72462cc7bd2d4d7a221
Yuanlong <yt4xb@virginia.edu>
2019-04-08 11:49:24 -0400

    modified:   ldm7-rtstats.sh

--- ldm7-rtstats.sh
+++ ldm7-rtstats.sh
@@ -2,8 +2,11 @@
 while true
 do
   LastTimeStamp=$(tail -1 var/logs/ldmd.log | awk '{print $1}')
-  sleep 60
+  DateStamp=$(echo $LastTimeStamp | sed 's/T.*$//g')
+  sleep 58
+  sed -n "/$LastTimeStamp/",'$p' var/logs/ldmd.log | sed -n '2,$p' >> ldmd-$DateStamp.log
   sed -n "/$LastTimeStamp/",'$p' var/logs/ldmd.log | grep -E "Received:|Inserted:" | sed -n '2,$p' >& ldm7.log
-  python util/ldm7-rtstats.py ldm7.log ldm7-rtstats.log NGRID
+  nl var/logs/ldmd.log | sed -n '2,$d'
+  python util/ldm7-rtstats.py ldm7.log ldm7-rtstats.log HDS
   ldmsend -v -h idc-uva.dynes.virginia.edu -P 38800 ldm7-rtstats.log
 done
