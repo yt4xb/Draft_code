73f52fbbc3de8d43b2f2c44e4d23ed2ef1e4e8ba
Yuanlong Tan <yt4xb@virginia.edu>
2019-05-24 14:22:25 -0400

    replace 'cat' with 'less'

--- LDM7/ldm7-rtstats.sh
+++ LDM7/ldm7-rtstats.sh
@@ -6,7 +6,7 @@ do
   echo $LastTimeStamp
   echo $DateStamp
   sleep 59
-  cat var/logs/ldmd.log | grep $DateStamp | grep -E "Received:|Inserted:" >& ldm7.log
+  less var/logs/ldmd.log | grep $DateStamp | grep -E "Received:|Inserted:" >& ldm7.log
   python util/ldm7-rtstats.py ldm7.log ldm7-rtstats.log HDS
   ldmsend -v -h idc-uva.dynes.virginia.edu -P 38800 ldm7-rtstats.log
 done
